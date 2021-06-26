0ad8f36187f3aeb2f1f4c58b0ee09ac2a5d929b7
simhallq <35776028+simhallq@users.noreply.github.com>
2021-06-26 17:00:53 +0200

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 import sys
 
-directory = "../data/"
+directory = "../data"
 user_data = directory + "/train"
 valid_data = directory + "/val"
 test_data = directory + "/label_book" # this can be the label book, or any other test set you create

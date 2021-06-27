f95a39f5f79125ab6c8bafeecf1bb9e6f45d2c65
simhallq <hallqvist.simon@gmail.com>
2021-06-27 22:07:26 +0200

    script to create summary pdfs of all datasets

---
+++ scripts/create_summary_pdfs.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+print()
+data_dir='../data/'
+for dir in os.listdir(f'{data_dir}train/'):
+    if not dir.startswith('.'):
+        os.system(f'cd {data_dir}train/{dir} && convert *.png {dir}_train.pdf && mv {dir}_train.pdf ../')
+
+for dir in os.listdir(f'{data_dir}val/'):
+    if not dir.startswith('.'):
+        os.system(f'cd {data_dir}val/{dir} && convert *.png {dir}_val.pdf && mv {dir}_val.pdf ../')
+
+for dir in os.listdir(f'{data_dir}label_book/'):
+    if not dir.startswith('.'):
+        os.system(f'cd {data_dir}label_book/{dir} && convert *.png {dir}_label_book.pdf && mv {dir}_label_book.pdf ../')
+
